49280b84e83542e2a1de55b3924fe91b3aa844f4
Henry Galvez <90292000+HenryGalvezQ@users.noreply.github.com>
2022-08-11 09:03:36 -0500

    todos los modelos registrados

--- src/Institucion/admin.py
+++ src/Institucion/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from Institucion.models import Alumno, Profesor, Cursos, Admin, Matriculas, Honorarios, Tutor
 # Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Profesor)
+admin.site.register(Cursos)
+admin.site.register(Admin)
+admin.site.register(Matriculas)
+admin.site.register(Honorarios)
+admin.site.register(Tutor)
