0ddc155f4412e13237f83030314be17b1369c75f
Henry Galvez <90292000+HenryGalvezQ@users.noreply.github.com>
2022-08-11 08:49:17 -0500

    tabla tutor creada

--- src/Institucion/models.py
+++ src/Institucion/models.py
@@ -1,3 +1,15 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+class Tutor(models.Model):
+    codigoTutor = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    tipoDocumento = models.CharField(max_length=50)
+    numDocumento = models.CharField(max_length=15)
+
+    def nombre_completo(self):
+        return "{}, {}".format(self.apellidos, self.nombres)
+    def __str__(self):
+        return self.nombre_completo()
