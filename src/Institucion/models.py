9f5b76bc4c7447a04dd1c4a860110ad810c7f981
stevenAnto <ecalcinap@unsa.edu.pe>
2022-08-11 08:54:42 -0500

    se creo modelo admin y profesor

--- src/Institucion/models.py
+++ src/Institucion/models.py
@@ -13,7 +13,7 @@ class Tutor(models.Model):
         return "{}, {}".format(self.apellidos, self.nombres)
     def __str__(self):
         return self.nombre_completo()
-    
+
 class Alumno(models.Model):
     codigoAlumno = models.AutoField(primary_key=True)
     nombres = models.CharField(max_length=50, verbose_name='Nombres')
@@ -29,3 +29,28 @@ class Alumno(models.Model):
         return "{}, {}".format(self.apellidos, self.nombres)
     def __str__(self):
         return self.nombre_completo()
+
+ class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    tipoUsuario = models.CharField(max_length=100)
+    def nombre_completo(self):
+        return "{}, {}".format(self.apellidos, self.nombres)
+    def __str__(self):
+        return self.nombre_completo()
+
+
+  class Profesor(models.Model):
+    codigoProfesor = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    tipoDocumento = models.CharField(max_length=50)
+    numDocumento = models.CharField(max_length=15)
+    def nombre_completo(self):
+    return "{}, {}".format(self.apellidos, self.nombres)
+def __str__(self):
+    return self.nombre_completo()
+
