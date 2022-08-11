fc79f2c4d3eac6f4e9ed9c2629f9f719c8a70b6f
Henry Galvez <90292000+HenryGalvezQ@users.noreply.github.com>
2022-08-11 08:50:01 -0500

    tabla alumno creada

--- src/Institucion/models.py
+++ src/Institucion/models.py
@@ -13,3 +13,19 @@ class Tutor(models.Model):
         return "{}, {}".format(self.apellidos, self.nombres)
     def __str__(self):
         return self.nombre_completo()
+    
+class Alumno(models.Model):
+    codigoAlumno = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=50, verbose_name='Nombres')
+    apellidos = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=150)
+    telefono = models.CharField(max_length=15)
+    tipoDocumento = models.CharField(max_length=50)
+    numDocumento = models.CharField(max_length=15)
+    fechaNacimiento = models.DateField()
+    codigoTutor = models.ForeignKey(Tutor, related_name='fk_alumno', on_delete=models.CASCADE)
+
+    def nombre_completo(self):
+        return "{}, {}".format(self.apellidos, self.nombres)
+    def __str__(self):
+        return self.nombre_completo()
